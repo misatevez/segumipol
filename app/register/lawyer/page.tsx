4fd2e82b63d38e6ccc10d49d929e520afae507db
Misael J <misatevez@gmail.com>
2024-04-25 18:21:51 -0300

    Update page.tsx

--- app/register/lawyer/page.tsx
+++ app/register/lawyer/page.tsx
@@ -5,11 +5,12 @@ import { fieldConfigs, especialidades } from "@/config/formFields";
 
 const RegistroAbogado = () => {
     return (
-      <FormularioRegistro tipo="abogado" especialidades={especialidades}>
-        {Object.values(fieldConfigs).map(config => (
-            <FormField key={config.id} {...config} />
-        ))}
-      </FormularioRegistro>
+      // <FormularioRegistro tipo="abogado" especialidades={especialidades}>
+      //   {Object.values(fieldConfigs).map(config => (
+      //       <FormField key={config.id} {...config} />
+      //   ))}
+      // </FormularioRegistro>
+      <div>Pendiente</div>
     );
 }
 
